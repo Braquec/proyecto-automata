80bda4b28293cfbba6c7eabc98577ae00cef1326
berch <berch@192.168.0.12>
2020-10-16 00:33:21 -0600

    First commit

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -20,25 +20,41 @@ public class Principal {
                
         String rutaDiccionario = lectura.leerDatos();
         int contador = 0, count = 0;
+        String nombreAutomataFile;
         
         try {
             
-            System.out.println("Cargando diccionario de datos....");
+            System.out.println("Cargando informacion del archivo...");
             Scanner input = new Scanner(new File(rutaDiccionario));  
+            Automata automataArchivo = new Automata();
             while (input.hasNext()) {
                 String line = input.nextLine();
                 
+                //Analizo la linea que corresponde
                 String[] letras = line.split(":");
-                
                 for (String letra: letras) {
                     letra = letra.trim();
-                    System.out.println(letra);
-                    if (contador == 2) {
-                        String[] detalles = letra.split(",");
+                    
+                    if(contador == 1){
+                        nombreAutomataFile = letra;
+                        if (ManejadorListas.obtenerAutomata(nombreAutomataFile) == null) {
+                            automataArchivo.setNombre(nombreAutomataFile);
+                            ManejadorListas.agregarAutomata(automataArchivo);
+                        }else{
+                            automataArchivo = ManejadorListas.obtenerAutomata(letra);
+                        }
+                        automataArchivo = ManejadorListas.obtenerAutomata(nombreAutomataFile);
+                    }else if(contador == 2) {
+                        String[] detalleEstados = letra.split(",");
+                        automataArchivo.setEstados(detalleEstados);
+                    }else if(contador == 4){
+                        if(ManejadorListas.obtenerAutomata(letra) == null){
+                            System.out.println("No existe este automata");
+                        }
+                    }else if(contador == 5){
+                        String[] simbolos = letra.split(",");
                         
-                        for (String detalle: detalles){
-                            System.err.println(detalle);
-                        }    
+                        //automataArchivo.
                     }
                     contador++;
 //                    Result res = new Result();
